3bbc3dd573304c3d214a6d759b377fcd4bc86d2c
Sihong Huang <sihongspring2011@gmail.com>
2019-01-18 12:19:59 -0500

    connect google oAuth to react frontend

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,11 @@
-const initialState = {
+import * as actionTypes from '../actions/types';
 
-}
 
-export default function(state = initialState, action) {
+export default function(state = null, action) {
   switch (action.type) {
+    case actionTypes.FETCH_USER:
+    // FETCH_USER action returns an object when there is a fetched user and an empty string when no user is found
+      return action.payload || false
     default:
       return state;
   }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,12 +5,18 @@ module.exports = (app) => {
     scope: ['profile', 'email']
   }));
   
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google'), 
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     // .logout is attached by passport
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   })
 
   app.get('/api/current_user', (req, res) => {
